3e7d8719177da0f3fa7f5f70bc1bbcf279cc8677
meisterfischy <grbmn@kstn.in>
2022-03-09 15:09:36 +0100

    Added job templates

--- app/Scheduler.hs
+++ app/Scheduler.hs
@@ -14,30 +14,29 @@ import Control.Monad (when)
 import Control.Lens
 import qualified Database.Persist.MySQL as MySQL
 
-data Playbook = Playbook {_name :: String, _playbookID :: Int}
-    deriving (Eq)
+-- Ich lese die User Jobs aus der Databse, erstelle Templates und darus Jobs die zu q hinzugefügt werden
+-- Außerdem prüfe ich ob die Config Repo ein update hat
+--   Ja   -> JobTemplate updaten und neue Instanzen bestimmen und damit die systemJobs ersetzen
+--   Nein -> Nichts machen
+-- Die fälligen user und systemjobs ausführen
+-- Bei system jobs neue instanz hizufügen und falls fail failcount erhöhen (execute -> catMaybes -> calculateNextInstance)
 
--- |A Job is an ansbile playbook which should be executed at a certain date with some optionale arguments 
---  and a fail counter
-data Job = Job {_timeDue :: UTCTime, _playbook :: Playbook, _optArgs:: [String], _failCount :: Int, _system :: Bool}
+data JobTemplate = JobTemplate {_jobName :: String, _scheduleFormat :: Schedule, _playbook :: String , _optArgs:: [String], _failCount :: Int}
+    deriving (Eq)
+-- A Job is an instance of a JobTemplate
+data Job = Job {_timeDue :: UTCTime, _template :: JobTemplate}
     deriving (Eq)
 type Jobs = [Job]
-data Queue = Queue {_dueJobs :: Jobs, _systemJobs :: Jobs, _userJobs :: Jobs}
+data Queue = Queue {_systemJobs :: Jobs, _userJobs :: Jobs}
+data Schedule = Foo
+    deriving (Eq)
 
-makeLenses ''Playbook
 makeLenses ''Job
+makeLenses ''JobTemplate
 makeLenses ''Queue
 
 instance Ord Job where
     compare j1 j2 = compare (_timeDue j1) (_timeDue j2)
-
-connectionInfo :: MySQL.ConnectInfo
-connectionInfo = MySQL.defaultConnectInfo 
-    { MySQL.connectHost     = "mdbtest-11.my.cum.re"
-    , MySQL.connectUser     = "hansible"
-    , MySQL.connectPassword = "AffqDbF2Vw5Aq7EHferw"
-    , MySQL.connectDatabase = "hansible"
-    }
 
 safeHead :: [a] -> Maybe a
 safeHead (x:xs) = Just x
@@ -50,25 +49,13 @@ insertSorted (y:ys) x = case compare x y of
                              _  -> x:y:ys
 
 -- |Inserts the items from the first job list into the second
---  Assumes that the first list is sorted
+--Assumes that the first list is sorted
 mergeJobs :: Jobs -> Jobs -> Jobs
 mergeJobs = foldl insertJob
 
 -- |Inserts a job in the job list, keeps the sorting
 insertJob :: Jobs -> Job -> Jobs
 insertJob = insertSorted
-
--- |Updates a job in a job list
---  A Job is identified by its id
-updateJob :: Jobs -> Job -> Jobs
-updateJob []     j = []
-updateJob (x:xs) j = if j `equiv` x then j:xs else x : updateJob xs j
-    where equiv j1 j2 = j1^.playbook.playbookID == j2^.playbook.playbookID
-
--- |Updates the first job list with the elements of the second
---  Assumes that the first list is sorted
-updateJobs :: Jobs -> Jobs -> Jobs
-updateJobs = foldl updateJob
 
 removeJob :: Job -> Jobs -> Jobs
 removeJob = delete
@@ -77,32 +64,42 @@ removeJob = delete
 splitJobsDue :: UTCTime -> Jobs -> (Jobs, Jobs)
 splitJobsDue time = span $ \job -> (job^.timeDue) <= time
 
-readPlaybookRepo :: IO Jobs
-readPlaybookRepo = undefined
+createUserJobs :: IO Jobs
+createUserJobs = undefined
 
-readDatabaseQueue :: IO Jobs
-readDatabaseQueue = undefined
+configRepoUpdate :: IO Bool
+configRepoUpdate = undefined
 
-calculateNextOccurence :: Playbook -> IO Job
-calculateNextOccurence = undefined
+readConfigRepo :: IO [JobTemplate]
+readConfigRepo = undefined
 
--- |Runs the ansible job and if the job is recurring or failed 3 times, returns the next instance of that job
---  Increases fail count if the job fails
+calculateNextInstance :: JobTemplate -> IO Job
+calculateNextInstance = undefined
+
+calculateNextInstances :: [JobTemplate] -> IO Jobs
+calculateNextInstances = mapM calculateNextInstance
+
+-- |Runs the ansible job and if the job is recurring and failed less than 3 times, returns the next instance of that job
+--Increases fail count if the job fails
 executeJob :: Job -> IO (Maybe Job)
 executeJob job = do
-    ret <- ansiblePlaybook "../ansible" (job^.playbook.name) (intercalate ";" job^.optArgs) ""  -- TODO: Lookup how to join optional arguments
-    if ret == 1 || job^.failCount == 3 then return Nothing else return $ Just $ job & failCount %~ (+1)
+    ret <- ansiblePlaybook "../ansible" (job^.template.playbook) (intercalate "," (job^.template.optArgs)) ""  -- TODO: Lookup how to join optional arguments
+    if ret == 1 || job^.template.failCount == 3 then return Nothing else return $ Just $ job & template.failCount %~ (+1)
 
-schedule :: IORef Queue -> IO ()
-schedule queueRef = do
+schedule :: IORef Queue -> [JobTemplate] -> IO ()
+schedule queueRef jobTempls = do
+    createUserJobs >>= \u -> modifyIORef queueRef (& userJobs %~ (`mergeJobs` u))
+
+    configRepoUpdate >>= \b -> when b $ do
+        newSysJobs <- readConfigRepo >>= calculateNextInstances
+        modifyIORef queueRef (& systemJobs .~ newSysJobs)
+
     time  <- getCurrentTime
-    readPlaybookRepo  >>= \repoJobs     -> modifyIORef queueRef (& systemJobs %~ (`updateJobs` repoJobs))
-    readDatabaseQueue >>= \databaseJobs -> modifyIORef queueRef (& userJobs   %~ (`mergeJobs`  databaseJobs))
-
     queue <- readIORef queueRef
-    let (ds,us) = splitJobsDue time (queue^.systemJobs)
-    let (du,uu) = splitJobsDue time (queue^.userJobs)
-    let queueDueJobs = queue^.dueJobs ++ mergeJobs ds du
+    let (dueUsr,upUsr) = splitJobsDue time (queue^.userJobs)
+    let (dueSys,upSys) = splitJobsDue time (queue^.systemJobs)
 
-    failedJobs <- mapM executeJob queueDueJobs <&> catMaybes
-    writeIORef queueRef $ Queue failedJobs us uu
+    failedJobs <- mapM executeJob dueSys <&> catMaybes
+    mapM_ executeJob dueUsr
+
+    writeIORef queueRef $ Queue (mergeJobs upSys failedJobs) upUsr
