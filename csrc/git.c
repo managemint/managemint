befc695641216c0ee3e673e2245ceff594999667
Jonas Gunz <himself@jonasgunz.de>
2022-03-10 18:07:12 +0100

    working-ish git prototype

--- csrc/git.c
+++ csrc/git.c
@@ -1,28 +1,112 @@
 #include "git.h"
 
+#include <alloca.h>
 #include <git2.h>
+#include <git2/branch.h>
 #include <git2/global.h>
+#include <git2/object.h>
+#include <git2/refs.h>
+#include <git2/remote.h>
 #include <git2/repository.h>
+#include <git2/strarray.h>
+
+#include <string.h>
+
+#define ASSERT_GIT_CALL(a,r) {if(a!=0){ret=r; goto error;}}
+
+#define _GIT_DEFAULT_REMOTE "origin"
 
 // https://libgit2.org/docs/guides/101-samples/
 
-int is_repo(char* _url, char* _path) {
-	if (git_repository_open_ext(NULL, _path, GIT_REPOSITORY_OPEN_NO_SEARCH, NULL) != 0) {
-		return -1;
+/* TODO
+ * - Sanitize paths
+ */
+
+int is_repo(char* _path, char* _remote) {
+	int ret = -2;
+	git_repository *repo = NULL;
+	git_strarray remotes = {0};
+	git_remote *remote = NULL;
+
+
+	git_libgit2_init();
+
+	ASSERT_GIT_CALL( git_repository_open_ext(&repo, _path, GIT_REPOSITORY_OPEN_NO_SEARCH, NULL), GIT_NOT_A_REPO );
+	ASSERT_GIT_CALL( git_remote_list(&remotes, repo), -1 );
+
+	if (remotes.count <= 0) {
+		ret = GIT_INCORRECT_REMOTE;
+		goto error;
 	}
-	return -1;
+
+	for (unsigned int i = 0; i < remotes.count; i++) {
+		ASSERT_GIT_CALL( git_remote_lookup(&remote, repo, remotes.strings[i]), GIT_INCORRECT_REMOTE );
+
+		if (strcmp(git_remote_url(remote), _remote) == 0) {
+			ret = GIT_OK;
+			break;
+		}
+	}
+
+end:
+	git_repository_free(repo);
+	git_strarray_free(&remotes);
+	git_remote_free(remote);
+
+	git_libgit2_shutdown();
+	return ret;
+error:
+	goto end;
 }
 
 int do_git_clone(char* _url, char* _path, char* _refspec) {
 	int ret = -1;
 	git_repository* repo = NULL;
+	git_clone_options clone_opts = GIT_CLONE_OPTIONS_INIT;
+
+	clone_opts.checkout_branch = _refspec;
 
 	git_libgit2_init();
 
-	ret = git_clone(&repo, _url, _path, NULL);
+	ASSERT_GIT_CALL(git_clone(&repo, _url, _path, &clone_opts), GIT_CALL_FAILED);
 
 end:
 	git_repository_free(repo);
+	git_libgit2_shutdown();
+	return ret;
+error:
+	// TODO delete directory if failed?
+	goto end;
+}
+
+int do_git_pull(char* _path, char* _refspec) {
+	int ret = -1;
+	git_repository *repo = NULL;
+	git_remote *remote = NULL;
+	//git_reference *ref = NULL;
+	git_oid oid;
+	git_object *obj = NULL;
+
+	git_libgit2_init();
+
+	ASSERT_GIT_CALL( git_repository_open_ext(&repo, _path, GIT_REPOSITORY_OPEN_NO_SEARCH, NULL), GIT_NOT_A_REPO );
+
+	ASSERT_GIT_CALL( git_remote_lookup(&remote, repo, _GIT_DEFAULT_REMOTE), GIT_INCORRECT_REMOTE );
+	ASSERT_GIT_CALL( git_remote_fetch(remote, NULL, NULL, NULL), GIT_FETCH_FAILED );
+
+	// Get object
+	// ASSERT_GIT_CALL( git_branch_lookup(&ref, repo, _refspec, GIT_BRANCH_REMOTE), GIT_CHECKOUT_FAILED);
+	ASSERT_GIT_CALL( git_reference_name_to_id(&oid, repo, _refspec), GIT_CHECKOUT_FAILED );
+	ASSERT_GIT_CALL( git_object_lookup(&obj, repo, &oid, GIT_OBJECT_ANY), GIT_CHECKOUT_FAILED );
+
+	ASSERT_GIT_CALL( git_checkout_tree(repo, obj, NULL), GIT_CHECKOUT_FAILED);
+
+error:
+end:
+	git_repository_free(repo);
+	git_remote_free(remote);
+	//git_reference_free(ref);
+	git_object_free(obj);
 	git_libgit2_shutdown();
 	return ret;
 }
--- csrc/git.h
+++ csrc/git.h
@@ -1,2 +1,14 @@
 #pragma once
 
+#define GIT_OK			0
+#define GIT_NOT_A_REPO		-1
+#define GIT_INCORRECT_REMOTE	-2
+#define GIT_CALL_FAILED		-3
+#define GIT_FETCH_FAILED	-4
+#define GIT_CHECKOUT_FAILED	-5
+
+int is_repo(char* _path, char* _remote);
+
+int do_git_clone(char* _url, char* _path, char* _refspec);
+
+int do_git_pull(char* _path, char* _refspec);
