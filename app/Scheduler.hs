0da2ea3b83c6d2fcc9d4cb3a8ece451f7cc5430f
meisterfischy <grbmn@kstn.in>
2022-03-10 19:09:22 +0100

    Small fixes and calculateNextInstance
    Changed time to time-compat since I needed firstDayOfWeekOnAfter, which
    sadly is hidden in time

--- app/Scheduler.hs
+++ app/Scheduler.hs
@@ -3,18 +3,23 @@
 module Scheduler where
 
 import Ansible
-import Data.Time.Clock
+import Data.Time.Compat
+import Data.Time.Calendar.Compat
+import Data.Time.LocalTime.Compat
+import Data.Time.Clock.Compat
 import Data.List (sort, intercalate)
 import Data.Maybe (isJust)
 import qualified Data.Map as M
 import Control.Lens
+import Control.Monad (when)
 import Control.Monad.State
 import Control.Monad.Trans
 
-data JobTemplate = JobTemplate {_scheduleFormat :: Schedule, _playbook :: String , _optArgs:: [String], _failCount :: Int, _systemJob :: Bool}
-data Job = Job {_timeDue :: UTCTime, _templateName :: String}
+data JobTemplate = JobTemplate {_scheduleFormat :: Schedule, _repoPath :: String, _playbook :: String, _failCount :: Int, _systemJob :: Bool}
+data Job = Job {_timeDue :: LocalTime, _templateName :: String}
     deriving (Eq)
-data Schedule
+data Schedule = Schedule {_scheduleDay :: [DayOfWeek], _scheduleTime :: Maybe ScheduleTime}
+data ScheduleTime = ScheduleTime {_startTime :: [TimeOfDay], _repetitionTime :: [Int]}
 
 type JobTemplates = M.Map String JobTemplate
 type Jobs = [Job]
@@ -24,37 +29,64 @@ instance Ord Job where
 
 makeLenses ''Job
 makeLenses ''JobTemplate
+makeLenses ''Schedule
+makeLenses ''ScheduleTime
 
+failMax = 3
+
+getTime :: IO LocalTime
+getTime = do
+    now <- getCurrentTime
+    timezone <- getCurrentTimeZone
+    return $ utcToLocalTime timezone now
+
+-- |Calculates the next job instances form the templates and removes the user job templates
 calculateNextInstances :: StateT JobTemplates IO Jobs
 calculateNextInstances = do
     templates <- get
-    time <- liftIO getCurrentTime
+    time <- liftIO getTime
     modify $ M.map removeUserJobTemplates <&> catMaybesMap
-    return $ sort $ map (calculateNextInstance time) $ M.toList templates
+    return $ map (calculateNextInstance time) $ M.toList templates
 
 catMaybesMap :: Ord k => M.Map k (Maybe v) -> M.Map k v
 catMaybesMap m = M.fromList $ foldr (\(k,v) l -> case v of {Just v' -> (k,v'):l; Nothing -> l}) [] (M.toList m)
 
-calculateNextInstance :: UTCTime -> (String,JobTemplate) -> Job
-calculateNextInstance = undefined
+calculateNextInstance :: LocalTime -> (String,JobTemplate) -> Job
+calculateNextInstance time (name,templ) = Job {_timeDue = dueTime (templ^.schedule.scheduleTime) daysThisWeek, _templateName = name}
+    where
+        daysThisWeek :: [Day]
+        daysThisWeek = concatMap (weekStartingAt (localDay time)) $ templ^.schedule.scheduleDay
+        dueTime :: Maybe ScheduleTime -> [Day] -> LocalTime
+        dueTime st days = case st of
+                            --                                             This are all possible LocalTimes
+                            Just schedTime -> minimum $ filter (>= time) $ LocalTime <$> days <*> scheduleTimeToList schedTime
+                            Nothing        -> LocalTime (days!!1) midnight
+
+scheduleTimeToList :: ScheduleTime -> [TimeOfDay]
+scheduleTimeToList = undefined
+
+-- |Calculates the first week beginning on a certain day of the week after a certain day
+weekStartingAt :: Day -> DayOfWeek -> [Day]
+weekStartingAt startDay weekDay = take 7 $ iterate succ $ firstDayOfWeekOnAfter weekDay startDay
 
 removeUserJobTemplates :: JobTemplate -> Maybe JobTemplate
 removeUserJobTemplates jt = if jt^.systemJob then Just jt else Nothing
 
 getDueJobs :: Jobs -> StateT JobTemplates IO Jobs
 getDueJobs jobs = do
-    time <- liftIO getCurrentTime
+    time <- liftIO getTime
     return $ takeWhile (\j -> j^.timeDue <= time) (sort jobs)
 
 executeJobs :: Jobs -> StateT JobTemplates IO ()
 executeJobs = mapM_ executeJob
 
--- TODO: Dont execute Job if failcount exeeds a certain value
 executeJob :: Job -> StateT JobTemplates IO ()
 executeJob job = do
     template <- get
-    success  <- liftIO $ (==1) <$> ansiblePlaybook "../ansible" (template ^. ix (job^.templateName) . playbook) (intercalate "," (template ^. ix (job^.templateName) . optArgs)) ""
-    put $ template & ix (job^.templateName) %~ (& failCount %~ modi success)
+    when (maybe False (\x -> x ^. failCount <= failMax) (template ^? ix (job^.templateName))) $ do  -- TODO: Change to exec
+        success  <- liftIO $ (==1) <$> ansiblePlaybook "../ansible" (template ^. ix (job^.templateName) . playbook) "" ""
+        let success = False
+        put $ template & ix (job^.templateName) %~ (& failCount %~ modi success)
         where
             modi :: Bool -> (Int -> Int)
             modi True  = const 0
@@ -71,3 +103,5 @@ runJobs :: JobTemplates -> IO JobTemplates
 runJobs jobTempls = do
     jobTempls' <- mappend <$> updateConfigRepoJobTemplates jobTempls <*> readJobsDatabase    -- somehow (++) doesn't work, therefore I used mappend
     snd <$> runStateT (calculateNextInstances >>= getDueJobs >>= executeJobs) jobTempls'
+
+schedule = undefined
