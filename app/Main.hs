ed34e273d20239ca416b6d46ecf1fb1bc62b8525
Paul Trojahn <paul.trojahn@gmail.com>
2022-03-15 14:42:20 +0100

    Log tree

--- app/Main.hs
+++ app/Main.hs
@@ -33,6 +33,7 @@ import Database.Persist.TH
 import Control.Monad.Logger (runStderrLoggingT)
 import Control.Monad.Reader
 import Control.Concurrent.Async
+import Data.Maybe
 
 data AddRepository = AddRepository
         { repoURL :: Text
@@ -66,15 +67,76 @@ instance YesodPersist App where
         App pool <- getYesod
         runSqlPool action pool
 
-getAllPlayIds :: Key Run -> ConnectionPool -> IO [String]
-getAllPlayIds run pool = do
-    xs <- runSqlPool (rawSql "SELECT DISTINCT event.play_id FROM event WHERE event.runId=?" [toPersistValue run] :: ReaderT SqlBackend IO [Single Text]) pool
-    pure (Prelude.map (unpack.unSingle) xs)
+eventToStatus :: Entity Event -> String
+eventToStatus (Entity eventid event) = "Success"
 
-getAllTaskIds :: Key Run -> Int -> ConnectionPool -> IO [String]
-getAllTaskIds run play pool = do
-    xs <- runSqlPool (rawSql "SELECT DISTINCT event.task_id FROM event WHERE event.runId=? AND event.play_id=?" [toPersistValue run, toPersistValue play] :: ReaderT SqlBackend IO [Single Text]) pool
-    pure (Prelude.map (unpack.unSingle) xs)
+hostWidget :: Entity Run -> Int -> Int -> String -> ConnectionPool -> Widget
+hostWidget (Entity runid run) playId taskId host pool = do
+    event <- runSqlPool (selectFirst [EventPlay_id ==. playId, EventTask_id ==. taskId, EventHost ==. host, EventRunId ==. runid] []) pool
+    toWidget
+        [whamlet|
+            <li>
+                #{host}: #{eventToStatus (Data.Maybe.fromJust event)}
+        |]
+
+getHosts :: MonadIO m => Key Run -> Int -> Int -> ReaderT SqlBackend m [Single String]
+getHosts run playId taskId = rawSql "SELECT DISTINCT event.host FROM event WHERE event.run_id=? AND event.play_id=? AND event.task_id=?" [toPersistValue run, toPersistValue playId,toPersistValue taskId]
+
+taskWidget :: Entity Run -> Int -> Int -> ConnectionPool -> Widget
+taskWidget entity@(Entity runid run) playId taskId pool = do
+    event <- runSqlPool (selectFirst [EventPlay_id ==. playId, EventTask_id ==. taskId, EventRunId ==. runid] []) pool
+    hosts <- runSqlPool (getHosts runid playId taskId) pool
+    toWidget
+        [whamlet|
+            <li>
+                #{eventTask (entityVal (Data.Maybe.fromJust event))}
+                <ul>
+                    $forall host <- hosts
+                        ^{hostWidget entity playId taskId (unSingle host) pool}
+        |]
+
+getTaskIds :: MonadIO m => Key Run -> Int -> ReaderT SqlBackend m [Single Int]
+getTaskIds run playId = rawSql "SELECT DISTINCT event.play_id FROM event WHERE event.run_id=? AND event.play_id=?" [toPersistValue run, toPersistValue playId]
+
+playWidget :: Entity Run -> Int -> ConnectionPool -> Widget
+playWidget entity@(Entity runid run) playId pool = do
+    event <- runSqlPool (selectFirst [EventPlay_id ==. playId, EventRunId ==. runid] []) pool
+    taskids <- runSqlPool (getTaskIds runid playId) pool
+    toWidget
+        [whamlet|
+            <li>
+                #{eventPlay (entityVal (Data.Maybe.fromJust event))}
+                <ul>
+                    $forall taskId <- taskids
+                        ^{taskWidget entity playId (unSingle taskId) pool}
+        |]
+
+getPlayIds :: MonadIO m => Key Run -> ReaderT SqlBackend m [Single Int]
+getPlayIds run = rawSql "SELECT DISTINCT event.play_id FROM event WHERE event.run_id=?" [toPersistValue run]
+
+runWidget :: Entity Run -> ConnectionPool -> Widget
+runWidget entity@(Entity runid run) pool = do
+    playids <- runSqlPool (getPlayIds runid) pool
+    toWidget
+        [whamlet|
+            <li>
+                #{runTriggerdate run}
+                <ul>
+                    $forall playid <- playids
+                        ^{playWidget entity (unSingle playid) pool}
+        |]
+
+playbookWidget :: Entity Playbook -> ConnectionPool -> Widget
+playbookWidget (Entity playbookid playbook) pool = do
+    runs <- runSqlPool (selectList [RunPlaybookId ==. playbookid] [Asc RunId]) pool
+    toWidget
+        [whamlet|
+            <li>
+                #{playbookPlaybookName playbook}
+                <ul>
+                    $forall entity <- runs
+                        ^{runWidget entity pool}
+        |]
 
 projectWidget :: Entity Project -> ConnectionPool -> Widget
 projectWidget (Entity projectid project) pool = do
@@ -89,12 +151,13 @@ projectWidget (Entity projectid project) pool = do
                     toWidget
                         [whamlet|
                             <li>
-                                Project: #{projectUrl project} (#{projectBranch project})
-                                <form method=post action=@{HomeR}>
-                                    ^{widgetDeleteRepo}
-                                    <button>Remove
-                                $forall entity <- playbooks
-                                    TestPlaybook
+                                <ul>
+                                    Project: #{projectUrl project} (#{projectBranch project})
+                                    <form method=post action=@{HomeR}>
+                                        ^{widgetDeleteRepo}
+                                        <button>Remove
+                                    $forall entity <- playbooks
+                                        ^{playbookWidget entity pool}
                         |]
                 err ->  [whamlet|
                             <font color="red">
