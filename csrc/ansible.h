492fa6eb92358f5289e22cdf780d8705752c68db
Jonas Gunz <himself@jonasgunz.de>
2022-03-16 12:47:09 +0100

    Ansible: error codes

--- csrc/ansible.h
+++ csrc/ansible.h
@@ -8,4 +8,13 @@
  *
 */
 
+#pragma once
+
+#define ANSIBLE_OK		0
+#define ANSIBLE_ERROR		1
+#define ANSIBLE_FAILED_HOSTS	2
+#define ANSIBLE_UNREACH_HOSTS	4
+#define ANSIBLE_BREAK_PLAY	8
+#define ANSIBLE_UNKNOWN		255
+
 int ansible( char* _path, char *_playbook, char *_limit, char *_tag );
