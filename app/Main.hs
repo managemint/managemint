e198b877f0e4afa5e3de96530d2b4f07d8c5cb04
Jonas Gunz <himself@jonasgunz.de>
2022-03-20 15:34:50 +0100

    Check for Webserver and Scheduler to be running

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {- app/Main.hs
  -
  - Copyright (C) 2022 Jonas Gunz, Konstantin Grabmann, Paul Trojahn
@@ -13,13 +14,36 @@ import Yesod
 import DatabaseUtil
 import Database.Persist
 import Database.Persist.MySQL
-import Control.Monad.Logger (runStderrLoggingT)
-import Control.Concurrent.Async
+import Control.Monad.Logger (runStderrLoggingT, runNoLoggingT)
+import Control.Monad (forever, when)
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (async, wait, poll, Async)
 import Webserver
+
+checkThreadOk :: (Show a, Show b) => Maybe (Either a b) -> IO Bool
+checkThreadOk (Just (Left e)) = do
+    print $ "Thread Exited with Exception " ++ show e
+    return False
+checkThreadOk (Just (Right r)) = do
+    print $ "Thread Exited normally with value " ++ show r
+    return False
+checkThreadOk Nothing = return True
+
+-- | Check status of list of Asyncs, exit when one fails
+-- to not drag along a half-crashed application
+monitorStatus :: (Show a) => [Async a] -> IO ()
+monitorStatus as = do
+    threadDelay 1000000
+    (mapM checkThreadOk =<< mapM poll as) >>= (`when` monitorStatus as) . and
+
+schedu = threadDelay 20000000
+websev = threadDelay 10000000
 
 main :: IO ()
 main = do
     runStderrLoggingT $ withMySQLPool connectionInfo 10 $ \pool -> liftIO $ do
         runSqlPersistMPool (runMigration migrateAll) pool
-        _ <- async $ schedule pool
-        runWebserver pool
+        sched <- async $ schedule pool
+        websv <- async $ runWebserver pool
+
+        monitorStatus [sched, websv]
